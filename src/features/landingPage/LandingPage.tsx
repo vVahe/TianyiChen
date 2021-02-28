0acb12154df59d149f608682d66c4dd5495faef8
vahe <vahe@hotmail.nl>
2021-02-28 23:16:34 +0100

    move LandingName component

--- src/features/landingPage/LandingPage.tsx
+++ src/features/landingPage/LandingPage.tsx
@@ -1,11 +1,13 @@
-import LandingName from '@components/LandingName';
-import React from 'react';
+import LandingName from '@components/landingName/LandingName';
+import React, { useState } from 'react';
 import './LandingPage.scss';
+import NavArrow from './../../components/navArrow/NavArrow';
 
 const LandingPage = () => {
     return (
         <div className="landing_page">
             <LandingName name="Tianyi Chen" />
+            <NavArrow />
         </div>
     );
 };
