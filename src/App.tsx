57d3e267ec906b6fd71510abf1d9b609de44d142
vvahe <vahe@hotmail.nl>
2021-03-11 20:30:30 -0800

    Add Router & TransitionsGroups

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import './App.scss';
 import LandingPage from './features/landingPage/LandingPage';
+import { Switch } from 'react-router';
+import BioPage from './features/bioPage/BioPage';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { Route, useLocation } from 'react-router-dom';
 
-function App() {
+const App = () => {
+    const location = useLocation();
+
     return (
         <div className="App">
-            <LandingPage />
+            <TransitionGroup>
+                <CSSTransition
+                    timeout={300}
+                    classNames="fade"
+                    key={location.key}
+                >
+                    <Switch location={location}>
+                        <Route exact path="bio">
+                            <BioPage title="T" subtitle="C" bio="hey" />
+                        </Route>
+                        <Route>
+                            <LandingPage />
+                        </Route>
+                    </Switch>
+                </CSSTransition>
+            </TransitionGroup>
         </div>
     );
-}
+};
 
 export default App;
--- src/components/navArrow/NavArrow.tsx
+++ src/components/navArrow/NavArrow.tsx
@@ -1,11 +1,13 @@
 import React, { ReactElement } from 'react';
-import './NavArrow.scss'; 
+import './NavArrow.scss';
 import NavArrowSVG from '@components/navArrow/NavArrowSVG';
 
 const NavArrow = () => {
-    return <div>
-        <NavArrowSVG />
-    </div>;
-}
+    return (
+        <div>
+            <NavArrowSVG />
+        </div>
+    );
+};
 
 export default NavArrow;
